8924df658175821702307dbe4a7726dabe546747
Chris Voitova <voitova@teamharbour.com>
2020-10-28 20:39:25 +0200

    added animation, which destroyed all

---
+++ cardsData.js
@@ -0,0 +1,34 @@
+let cards = [
+  {
+    name: "ghost",
+    src: "./img/ghost.png",
+  },
+  {
+    name: "hat",
+    src: "./img/hat.png",
+  },
+  {
+    name: "headstone",
+    src: "./img/headstone.png",
+  },
+  {
+    name: "jaw",
+    src: "./img/jaw.png",
+  },
+  {
+    name: "knife",
+    src: "./img/knife.png",
+  },
+  {
+    name: "pumpkin",
+    src: "./img/pumpkin.png",
+  },
+  {
+    name: "skull",
+    src: "./img/skull.png",
+  },
+  {
+    name: "zombie",
+    src: "./img/zombie.png",
+  },
+];
--- index.js
+++ index.js
@@ -1,84 +1,65 @@
-let cards = [
-  {
-    name: "ghost",
-    src: "./img/ghost.png",
-  },
-  {
-    name: "hat",
-    src: "./img/hat.png",
-  },
-  {
-    name: "headstone",
-    src: "./img/headstone.png",
-  },
-  {
-    name: "jaw",
-    src: "./img/jaw.png",
-  },
-  {
-    name: "knife",
-    src: "./img/knife.png",
-  },
-  {
-    name: "pumpkin",
-    src: "./img/pumpkin.png",
-  },
-  {
-    name: "skull",
-    src: "./img/skull.png",
-  },
-  {
-    name: "zombie",
-    src: "./img/zombie.png",
-  },
-];
-
 const doubleCards = (cards) => [...cards, ...cards.slice()];
 const shuffleCards = (cards) => cards.sort(() => 0.5 - Math.random());
 const field = document.querySelector(".field");
 
 cards = doubleCards(cards);
-
 shuffleCards(cards);
 
 function createCardsFromArr(arr) {
   let fragment = "";
   arr.forEach(({ name, src }) => {
-    return (fragment += `<div class="card" data-name="${name}"><img src="${src}"></div>`);
+    return (fragment += `<div class="flip-container" data-name="${name}">
+	 <div class="flipper">
+		 <div class="front">
+		 <img src="./img/pumpkin-front.png" class="front-img"/>
+		 </div>
+		 <div class="back">
+		 <div class="card"><img src="${src}" /></div>
+		 </div>
+	 </div>
+	</div>`);
   });
   field.innerHTML = fragment;
 }
 
 createCardsFromArr(cards);
-cardsLength = cards.length;
-console.log(cards.length);
+let cardsLength = cards.length;
 
 field.addEventListener("click", function (e) {
-  e.target.classList.add("active");
-  let checkedCards = [];
-  Array.from(field.children).forEach((item) => {
-    item.classList.contains("active") ? checkedCards.push(item) : null;
-  });
-  if (checkedCards.length === 2) {
-    const isEqual =
-      checkedCards[0].dataset.name === checkedCards[1].dataset.name;
-    if (isEqual) {
-      cardsLength -= 2;
-      checkedCards[0].style.opacity = 0;
-      checkedCards[0].classList.remove("active");
-      checkedCards[1].style.opacity = 0;
-      checkedCards[1].classList.remove("active");
-      checkedCards = [];
-    } else {
-      console.log(false);
+  const target = e.target;
+
+  if (target.classList.contains("flip-container")) {
+    target.classList.add("active");
+    target.classList.add("flipped");
+
+    let checkedCards = [];
+    cardsInField = Array.from(field.children);
+
+    cardsInField.forEach((item) => {
+      item.classList.contains("active") ? checkedCards.push(item) : null;
+    });
+    if (checkedCards.length === 2) {
+      checkedCards.reduce((first, curr, index, arr) => {
+        if (first.dataset.name === curr.dataset.name) {
+          cardsLength -= 2;
+          arr.forEach((item) => {
+            item.classList.remove("active", "card");
+            item.classList.add("hidden");
+          });
+          checkedCards = [];
+        } else {
+          arr.forEach((item) => item.classList.remove("active"));
+          checkedCards = [];
+        }
+      });
     }
-  }
-  if (cardsLength === 0) {
-    setTimeout(() => {
-      alert("u are win!");
-      shuffleCards(cards);
-      createCardsFromArr(cards);
-      cardsLength = cards.length;
-    }, 0);
+    if (cardsLength === 0) {
+      setTimeout(() => {
+        alert("u are win!");
+        shuffleCards(cards);
+        createCardsFromArr(cards);
+        cardsLength = cards.length;
+      });
+    }
   }
 });
